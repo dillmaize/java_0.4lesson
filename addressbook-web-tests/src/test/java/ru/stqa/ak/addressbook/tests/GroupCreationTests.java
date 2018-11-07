086ed8fb460a7edbbaca69edfdc1ce85ede0731e
Anna Kashenok <dillmaize@mail.ru>
2018-11-07 20:29:18 +0300

    assertEquals for groups added

--- addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/GroupCreationTests.java
@@ -1,17 +1,21 @@
 package ru.stqa.ak.addressbook.tests;
 
-import org.testng.annotations.*;
-import ru.stqa.ak.addressbook.model.GroupData;
+        import org.testng.Assert;
+        import org.testng.annotations.*;
+        import ru.stqa.ak.addressbook.model.GroupData;
 
 public class GroupCreationTests extends TestBase {
 
 
     @Test
-  public void testGroupCreation() throws Exception {
-    app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    app.logout();
-  }
+    public void testGroupCreation() throws Exception {
+        app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
+        app.logout();
+    }
 
 
 }
--- addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/GroupDeletionTests.java
@@ -1,20 +1,24 @@
 package ru.stqa.ak.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.ak.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase {
 
-  @Test
-  public void testGroupDeletion() throws Exception {
-   app.getNavigationHelper().gotoGroupPage();
-   if (! app.getGroupHelper().isThereAGroup()){
-        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-   }
-   app.getGroupHelper().selectGroup();
-   app.getGroupHelper().deleteSelectedGroups();
-   app.getGroupHelper().returnToGroupPage();
-   app.getNavigationHelper().gotoHomePage();
-  }
+    @Test
+    public void testGroupDeletion() throws Exception {
+        app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before - 1);
+        app.getNavigationHelper().gotoHomePage();
+    }
 
 }
