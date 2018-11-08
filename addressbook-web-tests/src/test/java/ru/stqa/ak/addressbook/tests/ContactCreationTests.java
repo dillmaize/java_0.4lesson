2b4e6f5a8858fed3af495471ab4a5eaabc129d0f
Anna Kashenok <dillmaize@mail.ru>
2018-11-08 20:42:04 +0300

    sorted for gropups added

--- addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/ContactCreationTests.java
@@ -3,12 +3,14 @@ package ru.stqa.ak.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.ak.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
-    app.getContactHelper().createContact(new ContactData("Anna", "Kashenok", "Saint Petersburg", "9999999", "qa@test.com", "test1"), true);
-    app.getContactHelper().returnToHomePage();
-    app.logout();
-  }
+    @Test
+    public void testContactCreation() throws Exception {
+        app.getContactHelper().createContact(new ContactData("Anna", "Kashenok", "Saint Petersburg", "9999999", "qa@test.com", "test1"), true);
+        app.getContactHelper().returnToHomePage();
+        app.logout();
+    }
 }
