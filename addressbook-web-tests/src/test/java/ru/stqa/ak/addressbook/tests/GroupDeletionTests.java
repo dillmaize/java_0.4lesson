236b2fd9ace04b030955d45c199f8afbabac88df
Anna Kashenok <dillmaize@mail.ru>
2018-11-08 18:10:02 +0300

    equals before, after for deletion group added

--- addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/GroupDeletionTests.java
@@ -19,8 +19,11 @@ public class GroupDeletionTests extends TestBase {
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
-        Assert.assertEquals(after, before.size() - 1);
-        app.getNavigationHelper().gotoHomePage();
+        Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
+
     }
 
 }
