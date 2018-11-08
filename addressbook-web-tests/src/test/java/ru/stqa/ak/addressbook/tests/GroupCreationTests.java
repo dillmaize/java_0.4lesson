41a383a0d2cfdadf2845a534e0bd27a5c7729f8b
Anna Kashenok <dillmaize@mail.ru>
2018-11-08 20:20:46 +0300

    lambda for group added

--- addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/ak/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ package ru.stqa.ak.addressbook.tests;
         import org.testng.annotations.*;
         import ru.stqa.ak.addressbook.model.GroupData;
 
+        import java.util.Comparator;
+        import java.util.HashSet;
         import java.util.List;
 
 public class GroupCreationTests extends TestBase {
@@ -13,10 +15,16 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() throws Exception {
         app.getNavigationHelper().gotoGroupPage();
         List<GroupData> before = app.getGroupHelper().getGroupList();
-        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        GroupData group = new GroupData("test2", null, null);
+        app.getGroupHelper().createGroup(group);
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
         app.logout();
+
+
+        group.setId(after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 
 }
