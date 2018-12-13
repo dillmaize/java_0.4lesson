65e5c55721e040019b097071faf8f6ae0d78bfbf
Anna Kashenok <dillmaize@mail.ru>
2018-12-13 18:41:52 +0300

    GitHub + Rest was added

---
+++ github-sample/src/test/java/ru/stqa/ak/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.ak.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("122c232d558e364ea562908f99aaf49b2ae73fd8");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("dillmaize", "java_0.4lesson")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
